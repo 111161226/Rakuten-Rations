0b2e6e0b02bc7edc16d91fdbdaeccd22083fd405
111161226 <fanrin9@gmail.com>
2024-08-07 15:39:17 +0900

    company registeration

---
+++ rakuten_team9/pages/api/registerOrganization/index.ts
@@ -0,0 +1,18 @@
+import prisma from '../../../lib/prisma';
+
+// POST /api/registerStock
+// Required fields in body: name
+// Required fields in body: num
+// Required fields in body: woman_ration
+export default async function handle(req, res) {
+    const { name, num, woman_ration } = req.body;
+    const result = await prisma.organization.create({
+        data: {
+            name: name,
+            num: num,
+            woman_ration: woman_ration
+        },
+    });
+    res.json(result);
+  }
+  
