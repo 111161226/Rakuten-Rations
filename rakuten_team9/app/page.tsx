4cef60848e2281785023201af32f5b6a97581a60
koteyeah <k.kokuta2346@gmail.com>
2024-08-07 17:13:50 +0900

    ヘッダーの定義

--- rakuten_team9/app/page.tsx
+++ rakuten_team9/app/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 import AdequacyCard from "@/components/elements/rations/AdequacyCard";
 import RationCard from "@/components/elements/rations/RationCard";
-import Header from "@/components/layouts/Header";
+import Header from "@/components/layouts/header";
 
 export default function Page() {
   return (
     <>
       <Header />
-      <RationCard />
       <AdequacyCard />
+      <RationCard />
+      <RationCard />
+      <RationCard />
     </>
   );
 }
