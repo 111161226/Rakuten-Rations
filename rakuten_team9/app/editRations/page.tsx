6c12ee679131bfdffed24457aeef5c4b7ba7a22a
HiyoriT <hiyori-t.01@keio.jp>
2024-08-07 20:26:43 +0900

    AddCategoryimage

--- rakuten_team9/app/editRations/page.tsx
+++ rakuten_team9/app/editRations/page.tsx
@@ -4,122 +4,6 @@
 // import { Box, Button, useDisclosure} from "@chakra-ui/react";
 // import { Spacer, Flex, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, FormControl, FormLabel, Input, Select, NumberInput, NumberInputField  } from "@chakra-ui/react";
 
-// export default function Home() {
-//   const { isOpen, onOpen, onClose } = useDisclosure();
-//   const [items, setItems] = useState([
-//     { category: "缶詰", expirationDate: "2025-12-31", initialQuantity: 10 },
-//     { category: "乾パン", expirationDate: "2024-06-15", initialQuantity: 5 },
-//   ]);
-//   const [quantity, setQuantity] = useState();
-//   const [newItem, setNewItem] = useState({
-//     category: "",
-//     expirationDate: "",
-//     initialQuantity: 0,
-//   });
-
-//   const handleQuantityChange = (e: any) => {
-//     setQuantity(e.target.value);
-//     setNewItem((prevItem) => ({
-//       ...prevItem,
-//       initialQuantity: Number(e.target.value),
-//     }));
-//   };
-
-//   const handleInputChange = (e: any) => {
-//     const { name, value } = e.target;
-//     setNewItem((prevItem) => ({
-//       ...prevItem,
-//       [name]: value,
-//     }));
-//   };
-
-//   const handleSubmit = () => {
-//     setItems((prevItems) => [...prevItems, newItem]);
-//     setNewItem({
-//       category: "",
-//       expirationDate: "",
-//       initialQuantity: 0,
-//     }); //初期化
-//     onClose();
-//   };
-
-//   const handleConfirm = () => {
-//     console.log(items);
-//   };
-
-//   return (
-//     <Box p="4">
-//       {items.map((item, index) => (
-//         <Item
-//           key={index}
-//           category={item.category}
-//           expirationDate={item.expirationDate}
-//           quantity={item.initialQuantity}
-//           handlefunction = {handleQuantityChange}
-//         />
-//       ))}
-
-//       <Flex>
-//         <Button onClick={onOpen} mr={2}>備蓄食を追加</Button>
-//         <Spacer />
-//         <Button onClick={handleConfirm}>確定</Button>
-//       </Flex>
-
-//       <Modal isOpen={isOpen} onClose={onClose}>
-//         <ModalOverlay />
-//         <ModalContent>
-//           <ModalHeader>新しいデータ</ModalHeader>
-//           <ModalCloseButton />
-//           <ModalBody>
-//             <FormControl id="category" mb={4}>
-//               <FormLabel>カテゴリ</FormLabel>
-//               <Select
-//                 name="category"
-//                 value={newItem.category}
-//                 onChange={handleInputChange}
-//                 placeholder='カテゴリを選ぶ'>
-//                 <option value='水'>水</option>
-//                 <option value='缶詰'>缶詰</option>
-//                 <option value='レトルト食品'>レトルト食品</option>
-//                 <option value='パックご飯'>パックご飯</option>
-//                 <option value='パン'>パン</option>
-//                 <option value='栄養補助食品'>栄養補助食品</option>
-//               </Select>
-//             </FormControl>
-//             <FormControl id="quantity" mb={4}>
-//               <FormLabel>個数</FormLabel>
-//               <Input
-//                 type="number"
-//                 name="initialQuantity"
-//                 value={quantity}
-//                 handleQuantityChange={handleQuantityChange}
-//                 index={index}
-//                 width="80px"
-//               />
-//             </FormControl>
-//             <FormControl id="expirationdate" mb={4}>
-//               <FormLabel>賞味期限
-//               </FormLabel>
-//               <Input
-//                 type="date"
-//                 name="expirationDate"
-//                 value={newItem.expirationDate}
-//                 onChange={handleInputChange}
-//                 placeholder='Select Date and Time' size='md'/>
-//             </FormControl>
-//           </ModalBody>
-
-//           <ModalFooter>
-//             <Button colorScheme='blue' mr={3} onClick={handleSubmit}>
-//               追加
-//             </Button>
-//             <Button onClick={onClose}>キャンセル</Button>
-//           </ModalFooter>
-//         </ModalContent>
-//       </Modal>
-//     </Box>
-//   );
-// }
 "use client";
 import Item from "@/components/elements/rations/RationItem";
 import { useState } from "react";
@@ -130,7 +14,7 @@ export default function Home() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [items, setItems] = useState([
     { category: "缶詰", expirationDate: "2025-12-31", initialQuantity: 10 },
-    { category: "乾パン", expirationDate: "2024-06-15", initialQuantity: 5 },
+    { category: "パン", expirationDate: "2024-06-15", initialQuantity: 5 },
   ]);
   
   const [newItem, setNewItem] = useState({
@@ -142,8 +26,8 @@ export default function Home() {
   const handleQuantityChange = (index: number, value: number) => {
     const updatedItems = items.map((item, i) =>
       i === index ? { ...item, initialQuantity: value } : item
-    );
-    setItems(updatedItems);
+    ); //変更したいindexと一致した場合にinitialQuantityを変更
+    setItems(updatedItems); //items更新
   };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
@@ -152,6 +36,11 @@ export default function Home() {
       ...prevItem,
       [name]: value,
     }));
+  };
+
+  const handleRemove = (index: number) => {
+    const updatedItems = items.filter((_, i) => i !== index); //indexと合わない要素だけを残す
+    setItems(updatedItems);
   };
 
   const handleSubmit = () => {
@@ -177,6 +66,7 @@ export default function Home() {
           expirationDate={item.expirationDate}
           quantity={item.initialQuantity}
           handleQuantityChange={handleQuantityChange}
+          handleRemove={handleRemove}
           index={index}
         />
       ))}
@@ -204,9 +94,9 @@ export default function Home() {
                 <option value="水">水</option>
                 <option value="缶詰">缶詰</option>
                 <option value="レトルト食品">レトルト食品</option>
-                <option value="ごはん">ごはん</option>
-                <option value="パン系">パン系</option>
-                <option value="栄養補助食">栄養補助食</option>
+                <option value="パックご飯">パックご飯</option>
+                <option value="パン">パン</option>
+                <option value="栄養補助食品">栄養補助食品</option>
               </Select>
             </FormControl>
             <FormControl id="quantity" mb={4}>
--- rakuten_team9/components/elements/rations/RationItem.tsx
+++ rakuten_team9/components/elements/rations/RationItem.tsx
@@ -2,55 +2,44 @@
 // import { useState } from "react";
 // import { Box, Text, Input, Flex, Spacer, Button, useDisclosure } from "@chakra-ui/react";
 
-
-//   const Item = ({ category, expirationDate, quantity, handleQuantityChange, index }: any) => {
-//     const onQuantityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//       handleQuantityChange(index, Number(e.target.value));
-//     };
-//   return (
-//     <Box borderWidth="1px" borderRadius="lg" overflow="hidden" p="6" mb="4">
-//       <Flex>
-//         <Box>
-//           <Text fontSize="xl" fontWeight="bold">
-//             {category}
-//           </Text>
-//           <Text color="gray.500">賞味期限: {expirationDate}</Text>
-//         </Box>
-//         <Spacer />
-//         <Box textAlign="right">
-//           <Text fontSize="md">個数:</Text>
-//           <Input
-//             type="number"
-//             value={quantity}
-//             onChange={onQuantityChange}
-//             width="80px"
-//           />
-//         </Box>
-//       </Flex>
-//     </Box>
-//   );
-// };
-
-// export default Item;
-
-import { Box, Text, Input, Flex, Spacer } from "@chakra-ui/react";
+import { Box, Text, Input, Flex, Spacer, Image, Button} from "@chakra-ui/react";
 
 type ItemProps = {
   category: string;
   expirationDate: string;
   quantity: number;
   handleQuantityChange: (index: number, value: number) => void;
+  handleRemove: (index: number) => void;
   index: number;
 };
 
-const Item = ({ category, expirationDate, quantity, handleQuantityChange, index }: ItemProps) => {
+const Item = ({ category, expirationDate, quantity, handleQuantityChange, handleRemove, index }: ItemProps) => {
   const onQuantityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     handleQuantityChange(index, Number(e.target.value));
   };
+  const onRemoveClick = () => {
+    handleRemove(index);
+  };
+
+  let imageUrl: string;
+  if (category == "水") {
+    imageUrl = "categoryImages/petbottle_water_full.png";
+  } else if (category == "パックご飯") {
+    imageUrl = "categoryImages/pack_gohan_renji.png";
+  } else if (category == "パン") {
+    imageUrl = "categoryImages/pan_bread_set.png";
+  } else if (category == "缶詰") {
+    imageUrl = "categoryImages/food_kandume.png";
+  } else if (category == "レトルト食品") {
+    imageUrl = "categoryImages/retort_food.png";
+  } else {
+    imageUrl = "categoryImages/food_energy_bar.png";
+  }
 
   return (
     <Box borderWidth="1px" borderRadius="lg" overflow="hidden" p="6" mb="4">
       <Flex>
+        <Image src={imageUrl} alt="カテゴリ画像" boxSize="100px" mr={4} />
         <Box>
           <Text fontSize="xl" fontWeight="bold">
             {category}
@@ -58,7 +47,7 @@ const Item = ({ category, expirationDate, quantity, handleQuantityChange, index 
           <Text color="gray.500">賞味期限: {expirationDate}</Text>
         </Box>
         <Spacer />
-        <Box textAlign="right">
+        <Box textAlign="right" mr={4}>
           <Text fontSize="md">個数:</Text>
           <Input
             type="number"
@@ -67,6 +56,9 @@ const Item = ({ category, expirationDate, quantity, handleQuantityChange, index 
             width="80px"
           />
         </Box>
+        <Button colorScheme="red" onClick={onRemoveClick} mt={2}>
+          削除
+        </Button>
       </Flex>
     </Box>
   );
