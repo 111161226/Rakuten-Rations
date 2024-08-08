3b647c3bcdbf2fdcf5f8aa3b4a31c88211d58af4
111161226 <fanrin9@gmail.com>
2024-08-08 10:34:13 +0900

    change type of date to return

--- rakuten_team9/pages/api/gettest/index.ts
+++ rakuten_team9/pages/api/gettest/index.ts
@@ -25,7 +25,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             "category": food.category,
             "quantity": food.num,
             "index": i,
-            "expirationDate": food.expired_at
+            "expirationDate": food.expired_at.toString()
         }
         Customized.push(custom)
         i++;
